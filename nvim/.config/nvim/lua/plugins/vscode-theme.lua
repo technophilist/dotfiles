9cb579b61d6081dde90b40675902fe939ea797e8
technophilist <54663474+technophilist@users.noreply.github.com>
2026-04-21 15:39:11 -0400

    fix incorrect tsdoc highlight color for vscode theme

--- nvim/.config/nvim/lua/plugins/vscode-theme.lua
+++ nvim/.config/nvim/lua/plugins/vscode-theme.lua
@@ -1,11 +1,13 @@
 return {
-  "Mofiqul/vscode.nvim",
-  priority = 1000,
-  config = function()
-    require("vscode").setup({
-      transparent = false,
-      italic_comments = true,
-    })
-    vim.cmd.colorscheme("vscode")
-  end,
+	"Mofiqul/vscode.nvim",
+	priority = 1000,
+	config = function()
+		require("vscode").setup({
+			transparent = false,
+			italic_comments = true,
+		})
+		vim.cmd.colorscheme("vscode")
+		-- ensures ts doc comments have proper highlighting
+		vim.api.nvim_set_hl(0, "typescriptDocComment", { link = "Comment" })
+	end,
 }
