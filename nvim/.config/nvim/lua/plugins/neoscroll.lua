8a69377cab5a60631251bcce5b14091cdff76f08
technophilist <54663474+technophilist@users.noreply.github.com>
2026-05-16 21:27:51 -0400

    add neoscroll plugin

---
+++ nvim/.config/nvim/lua/plugins/neoscroll.lua
@@ -0,0 +1,9 @@
+return {
+	"karb94/neoscroll.nvim",
+	opts = {
+		mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>" },
+		hide_cursor = false,
+		easing = "quadratic",
+		duration_multiplier = 0.5,
+	},
+}
